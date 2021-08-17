9aa69ad708a3478945f0a2a597cd77772ba3a666
warkamania <87486508+warkamania@users.noreply.github.com>
2021-08-17 17:59:58 +0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import PostList from './components/PostList'
+//import ClassCounter from './components/ClassCounter'
+//import Counter from './components/Counter'
+import './styles/App.css'
 function App() {
+   const [posts, setPots] = useState([
+    {id: 1, title: 'Javascript 1',body: 'Description'},
+    {id: 2, title: 'Javascript 2',body: 'Description'},
+    {id: 3, title: 'Javascript 3',body: 'Description'}
+   ])
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostList posts={posts}/>
     </div>
-  );
+  )
 }
 
 export default App;
