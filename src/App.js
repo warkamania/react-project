963bde4dd2283898b0cf9fd46f81857a0ea4e3a0
warkamania <87486508+warkamania@users.noreply.github.com>
2021-08-19 17:56:24 +0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import React, { useState } from 'react'
+import React, {  useState } from 'react'
 import PostList from './components/PostList'
 //import ClassCounter from './components/ClassCounter'
 //import Counter from './components/Counter'
 import './styles/App.css'
-function App() {
-   const [posts, setPots] = useState([
-    {id: 1, title: 'Javascript 1',body: 'Description'},
-    {id: 2, title: 'Javascript 2',body: 'Description'},
-    {id: 3, title: 'Javascript 3',body: 'Description'}
-   ])
+//import MyButton from './components/UI/Button/MyButton'
+//import MyInput from './components/UI/Input/MyInput'
+import PostForm from './components/PostForm'
+import MySelect from './components/UI/Select/MySelect'
 
-  
-  
+function App() {
+   const [posts, setPosts] = useState([
+    {id: 1, title: 'aaa 1',body: 'sds'},
+    {id: 2, title: 'sds 2',body: 'vlkdd'},
+    {id: 3, title: 'sdssd 3',body: 'fgf'}
+   ])
+   const [selectedSort, setSelectedSort] = useState('')
+
+   const createPost = (newPost) =>{
+     setPosts([...posts,newPost])
+   } 
+   
+   const removePost = (post) =>{
+     setPosts(posts.filter(p =>p.id !== post.id))
+   }
+   
+   const sortPost = (sort) =>{
+     setSelectedSort(sort)
+     setPosts([...posts].sort((a,b)=>a[sort].localeCompare(b[sort])))
+
+   }
   return (
     <div className="App">
-      <PostList posts={posts}/>
+      <PostForm create={createPost}/>
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPost}
+          defaultValue="Сортировка"
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'}
+          ]}
+        />
+      </div>
+      {posts.length !==0
+        ? <PostList remove={removePost} posts={posts} title="Список постов"/>
+        : <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
+      }
+      
     </div>
   )
 }
